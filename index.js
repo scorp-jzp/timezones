b6625f40620efea27f1a8e3701eebc4d9ccf57b1
Scorp <scorpstark@gmail.com>
2024-07-14 22:39:56 -0300

    update index.js

--- index.js
+++ index.js
@@ -28,18 +28,20 @@ function updateCity(event) {
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
     citiesElement.innerHTML = `
-        <div class="city">
-            <div>
-                <h2>${cityName}</h2>
-                <div class="date">${cityTime.format("MMMM  Do YYYY")}</div>
-                <div class="time">${cityTime.format("h:mm:ss A")}</div>
-            </div>
-        </div>
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+      "A"
+    )}</small></div>
+    </div>
     `;
-}
-
-updateTime();
-setInterval(updateTime, 1000);
-
-let citiesSelectElement = document.querySelector("#city");
-citiesSelectElement.addEventListener("change", updateCity);
+  }
+  
+  updateTime();
+  setInterval(updateTime, 1000);
+  
+  let citiesSelectElement = document.querySelector("#city");
+  citiesSelectElement.addEventListener("change", updateCity);
