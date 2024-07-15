16cad909afbd5710e45042f9cf494eef0affd5ee
Scorp <scorpstark@gmail.com>
2024-07-14 22:21:26 -0300

    Update to ch5

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
 function updateTime() {
-    // New York
-    let newYorkElement = document.querySelector("#new-york");
-    if (newYorkElement) {
-        let newYorkDateElement = newYorkElement.querySelector(".date");
-        let newYorkTimeElement = newYorkElement.querySelector(".time");
-        let newYorkTime = moment().tz("America/New_York");
-
-        newYorkDateElement.innerHTML = newYorkTime.format("dddd, MMMM Do YYYY");
-        newYorkTimeElement.innerHTML = newYorkTime.format("h:mm:ss A");
-    }
-
-    // London
-    let londonElement = document.querySelector("#london");
-    if (londonElement) {
-        let londonDateElement = londonElement.querySelector(".date");
-        let londonTimeElement = londonElement.querySelector(".time");
-        let londonTime = moment().tz("Europe/London");
-
-        londonDateElement.innerHTML = londonTime.format("dddd, MMMM Do YYYY");
-        londonTimeElement.innerHTML = londonTime.format("h:mm:ss A");
-    }
-
     // São Paulo
     let saoPauloElement = document.querySelector("#sao-paulo");
     if (saoPauloElement) {
@@ -35,9 +13,6 @@ function updateTime() {
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
-    if (cityTimeZone === "current") {
-        cityTimeZone = moment.tz.guess();
-    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
